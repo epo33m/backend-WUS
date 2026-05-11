4918f17b022b3b7f2e7f778618d067341eb48050
StrongMothersChild <strongmotherschild@gmail.com>
2026-05-11 23:06:17 +0800

    feat: complete issue #3 Loyaltytransaction entity (INSERT ONLY)

---
+++ src/loyalty/entities/loyalty-transaction.entity.ts
@@ -0,0 +1,43 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    Index,
+} from 'typeorm';
+
+export enum LoyaltyTransactionReason {
+    PURCHASE = 'PURCHASE',
+    REDEMPTION = 'REDEMPTION',
+    ADMIN_ADJUST = 'ADMIN_ADJUST',
+    REFUND = 'REFUND',
+}
+
+@Entity('loyalty_transactions')
+export class LoyaltyTransaction {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Index()
+    @Column()
+    userId: number;
+
+    @Column({
+    nullable: true,
+    })
+    orderId: number;
+
+    @Column({
+    type: 'int',
+    })
+    delta: number;
+
+    @Column({
+    type: 'enum',
+    enum: LoyaltyTransactionReason,
+    })
+    reason: LoyaltyTransactionReason;
+
+    @CreateDateColumn()
+    createdAt: Date;
+}
--- src/loyalty/loyalty.module.ts
+++ src/loyalty/loyalty.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coupon } from './entities/coupon.entity';
 import { TierConfig } from './entities/tier-config.entity';
+import { LoyaltyTransaction } from './entities/loyalty-transaction.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Coupon, TierConfig])],
+    imports: [TypeOrmModule.forFeature([Coupon, TierConfig, LoyaltyTransaction])],
     exports: [TypeOrmModule],
 })
 export class LoyaltyModule {}
